fb419f1c1c26cda85d97dee437187292ee1d1d0a
Cesar Augusto <cesaraugustoc@hotmail.com>
2025-03-18 10:39:05 -0300

    User insert

--- src/main/java/com/cesaraugusto/course/resource/UserResource.java
+++ src/main/java/com/cesaraugusto/course/resource/UserResource.java
@@ -1,16 +1,21 @@
 package com.cesaraugusto.course.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cesaraugusto.course.entities.User;
 import com.cesaraugusto.course.services.UserService;
+
 
 
 @RestController
@@ -31,6 +36,14 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 	
 
 }
