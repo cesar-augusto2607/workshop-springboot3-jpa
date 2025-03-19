4fc71d3bf0921a063bc8ba0068637d8080f572ce
Cesar Augusto <cesaraugustoc@hotmail.com>
2025-03-19 10:43:33 -0300

    Exception delete

--- src/main/java/com/cesaraugusto/course/resource/exceptions/StandardError.java
+++ src/main/java/com/cesaraugusto/course/resource/exceptions/StandardError.java
@@ -21,7 +21,6 @@ public class StandardError implements Serializable {
 	}
 
 	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
-		super();
 		this.timestamp = timestamp;
 		this.status = status;
 		this.error = error;
